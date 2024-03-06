1155fc75e909896b44588e3dcf6cf995d3b98533
Kelvin <domainkelvin99@gmail.com>
2024-03-06 17:58:34 +0800

    tested ok

--- App.js
+++ App.js
@@ -1,42 +1,51 @@
-import React, { useState } from 'react';
-import { View, Text, Button } from 'react-native';
+import React from 'react';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 import * as LocalAuthentication from 'expo-local-authentication';
 
 export default function App() {
-  const [isFaceAuthenticationSupported, setIsFaceAuthenticationSupported] = useState(false);
-
-  const checkFaceAuthenticationSupport = async () => {
-    const hasHardware = await LocalAuthentication.hasHardwareAsync();
-    const supportedTypes = await LocalAuthentication.supportedAuthenticationTypesAsync();
-
-    setIsFaceAuthenticationSupported(hasHardware && supportedTypes.includes(LocalAuthentication.AuthenticationType.FACIAL_RECOGNITION));
-    console.log(isFaceAuthenticationSupported);
-  };
-
-  const handleFaceAuthentication = async () => {
+  const onFaceId = async () => {
     try {
-      const result = await LocalAuthentication.authenticateAsync({
-        promptMessage: 'Unlock your device',
-        disableDeviceFallback: true,
-        authenticationType: LocalAuthentication.AuthenticationType.FACIAL_RECOGNITION,
-      });
-
-      if (result.success) {
-        console.log('Authentication succeeded');
-        // Allow access to the app or navigate to the authenticated screen
-      } else {
-        console.log('Authentication failed');
+      const test = await LocalAuthentication.supportedAuthenticationTypesAsync();
+      console.log(test); 
+      // Checking if device is compatible
+      const isCompatible = await LocalAuthentication.hasHardwareAsync();
+      
+      if (!isCompatible) {
+        throw new Error('Your device isn\'t compatible.')
       }
+
+      // Checking if device has biometrics records
+      const isEnrolled = await LocalAuthentication.isEnrolledAsync();
+      
+      if (!isEnrolled) {
+        throw new Error('No Faces / Fingers found.')
+      }
+
+      // Authenticate user
+      await LocalAuthentication.authenticateAsync(2);
+
+      Alert.alert('Authenticated', 'Welcome back !')
     } catch (error) {
-      console.error('Authentication error:', error);
+      Alert.alert('An error as occured', error?.message);
     }
   };
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Face Recognition Unlock</Text>
-      <Button title="Check Face Authentication Support" onPress={checkFaceAuthenticationSupport} />
-      {isFaceAuthenticationSupported && <Button title="Unlock with Face Recognition" onPress={handleFaceAuthentication} />}
+    <View style={styles.container}>
+      <Text style={styles.text}>That is a simple example on how to use Face ID / Touch ID on React Native!</Text>
+      <Button title="Sign in with Face ID" onPress={onFaceId} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    textAlign: 'center',
+  }
+});
